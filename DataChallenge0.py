553f6867aaa5c3b0bb268f3a0924a001bea10671
ducksarecool2 <abarnet3@mail.umw.edu>
2018-02-15 16:59:22 -0500

    Here ya go

---
+++ DataChallenge0.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 15:56:48 2018
+
+@author: bryceanderson
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+import json
+with open('nobel.json', 'r') as f:
+    nobel = json.load(f)
+names = []
+year = []
+cat = []
+share = []
+
+for prizes in nobel['prizes']:
+    for i in range(len(prizes['laureates'])):
+        cat.append(prizes['category'])
+        year.append(prizes['year'])
+        names.append(prizes['laureates'][i]['firstname'] +" "+ prizes['laureates'][i]['surname'])
+        share.append("1/" + prizes['laureates'][i]['share'])
+
+nobel_prizes = pd.DataFrame({'category': cat, 'year': year, 'share': share}, index = names)
+#%%-----------------------------------------
+Born = []
+BornC = []
+BornCC = []
+Died = []
+DiedC = []
+DiedCC = []
+Name = []
+Gender = []
+AwardCity = []
+AwardCountry = []
+AwardCategory = []
+Motivation = []
+AwardShare = []
+AwardYear = []
+
+for laureate in nobel['laureates']:
+    Born.append(laureate['born'])
+    BornC.append(laureate['bornCountry'])
+    BornCC.append(laureate['bornCountryCode'])
+    Died.append(laureate['died'])
+    DiedC.append(laureate['diedCity'])
+    DiedCC.append(laureate['diedCountryCode'])
+    Name.append(laureate['firstname'])
+    Gender.append(laureate['gender'])
+    for i in range(len(laureate['prizes'])):
+        Motivation.append(laureate['prizes'][i]['motivation'])
+print(Motivation)
